092254d35a9df201fdb37e75634ebfba23a3cd56
shg <shg@linkedin.com>
2023-11-08 21:10:12 +0530

    Valid Parentheses

---
+++ code/arrays/ValidParentheses.java
@@ -0,0 +1,37 @@
+package code.arrays;
+
+import java.util.Stack;
+
+
+public class ValidParentheses {
+  public static void main(String[] args) {
+    String str = "]";
+    System.out.println("The Parentheses in the string are " + (isValid(str) ? "Valid" : "Invalid"));
+  }
+  public static boolean isValid(String s) {
+    char[] chars = s.toCharArray();
+    Stack<Character> stack = new Stack<>();
+    for (char ch: chars) {
+      if (ch == '(' || ch == '[' || ch == '{' ) {
+        stack.push(ch);
+      } else {
+        if (stack.isEmpty()) return false;
+        char top = stack.pop();
+        switch (ch) {
+          case ')' :
+            if (top != '(') return false;
+            break;
+
+          case ']' :
+            if (top != '[') return false;
+            break;
+
+          case '}' :
+            if (top != '{') return false;
+            break;
+        }
+      }
+    }
+    return stack.isEmpty();
+  }
+}
