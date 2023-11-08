68b3d9f9ba665ea550cac2592dac3e236cb57250
shg <shg@linkedin.com>
2023-11-08 18:12:45 +0530

    Test out basic miscellaneous stuff

---
+++ code/arrays/TestStuff.java
@@ -0,0 +1,14 @@
+package code.arrays;
+
+public class TestStuff {
+  public static void main(String[] args) {
+    Increment();
+  }
+  public static void Increment(){
+    for (int i=0;i<=10;i++) {
+      System.out.println("i values is "+i);
+      //int x = i++;
+      System.out.println("x values is "+ ++i);
+    }
+  }
+}
