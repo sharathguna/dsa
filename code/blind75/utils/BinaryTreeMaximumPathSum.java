a0876fbe890838626d67704581f93557b50e1ba0
shg <shg@linkedin.com>
2023-11-27 20:05:50 +0530

    Binary Tree Maximum Path Sum

--- code/blind75/utils/BinaryTreeMaximumPathSum.java
+++ code/blind75/utils/BinaryTreeMaximumPathSum.java
@@ -20,7 +20,6 @@ public class BinaryTreeMaximumPathSum {
     if (node == null) return 0;
     int left = Math.max(0, maxPathSumHelper(node.left));
     int right = Math.max(0, maxPathSumHelper(node.right));
-
     maxPathSumVal = Math.max(maxPathSumVal, left + right + node.val);
     return Math.max(left, right) + node.val;
   }
