83e24d34de75112e6aef9fb0c98f792f3a7779a9
shg <shg@linkedin.com>
2024-01-02 20:22:10 +0530

    Maximum Width Of Binary Tree

---
+++ code/all/MaximumWidthOfBinaryTree.java
@@ -0,0 +1,54 @@
+package code.all;
+
+import code.utils.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class MaximumWidthOfBinaryTree {
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(3);
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
+    root.right = new TreeNode(2);
+    root.right.right = new TreeNode(9);
+    System.out.println("The maximum width in the binary tree is : " + widthOfBinaryTree(root) );
+  }
+  public static int widthOfBinaryTree(TreeNode root) {
+    if (root == null) return 0;
+    Queue<Pair<TreeNode,Integer>> queue = new LinkedList<>();
+    queue.add(new Pair<>(root,1));
+    int maxWidth = Integer.MIN_VALUE;
+    while (!queue.isEmpty()) {
+      int levelLength = queue.size();
+      int left = queue.peek().getValue();
+      int rootInt = left;
+      while (levelLength-- > 0) {
+        Pair<TreeNode, Integer> curr = queue.poll();
+        TreeNode node = curr.getKey();
+        rootInt = curr.getValue();
+        if (node.left != null) queue.add(new Pair<>(node.left,rootInt*2));
+        if (node.right != null) queue.add(new Pair<>(node.right,rootInt*2 + 1));
+      }
+      maxWidth = Math.max(maxWidth, rootInt - left + 1);
+    }
+    return maxWidth;
+  }
+  static class Pair<T,S> {
+    T t;
+    S s;
+
+    public Pair(T t, S s) {
+      this.t = t;
+      this.s = s;
+    }
+
+    T getKey() {
+      return t;
+    }
+    S getValue() {
+      return s;
+    }
+  }
+}
