19f746db570ab7f0c4f1bed0d3ff2efbc4560b77
shg <shg@linkedin.com>
2024-01-18 21:41:29 +0530

    Adapter Pattern

---
+++ code/designpattern/structural/adapter.java
@@ -0,0 +1,32 @@
+package code.designpattern.structural;
+
+interface JsonLogger {
+  void logMessage(String message);
+}
+
+class XmlLogger {
+  public void log(String xmlMessage) {
+    System.out.println(xmlMessage);
+  }
+}
+
+class LoggerAdapter implements JsonLogger {
+  private final XmlLogger xmlLogger;
+
+  public LoggerAdapter(XmlLogger xmlLogger) {
+    this.xmlLogger = xmlLogger;
+  }
+
+  @Override
+  public void logMessage(String message) {
+    xmlLogger.log(message);
+  }
+}
+
+class Adapter {
+  public static void main(String[] args) {
+    JsonLogger logger = new LoggerAdapter(new XmlLogger());
+    logger.logMessage("<message>hello</message>");
+  }
+}
+
